4776f3a1288b0eaf9b0087451d4f18972172db22
Devacius <devaciusaboveall789456@gmail.com>
2024-02-27 16:42:23 +0530

    completing the both backend and frontend part from my end

--- paytmproject/frontend/frontend/src/components/Signup.jsx
+++ paytmproject/frontend/frontend/src/components/Signup.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
- import { FirstName } from '../atoms/FirstName';
- import { SecondName } from '../atoms/SecondName';
- import { Password } from '../atoms/Password';
- import { Username } from '../atoms/Username';
- import { RecoilRoot,useRecoilState } from 'recoil';
+
+import React, { useState } from 'react';
+import { Navigate } from "react-router-dom";
+import { Axios } from 'axios';
 import { Link } from 'react-router-dom';
 
 export default function Signup() {
-
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+ 
   return (
     <div className='flex flex-col justify-center bg-slate-500 relative top-8 w-72 rounded-lg '>
-      <div>
+      <div className='flex  flex-col items-center p-2'>
         <p className='text-black font-sans-serif text-2xl font-bold'>
           Sign Up
         </p>
@@ -19,15 +21,46 @@ export default function Signup() {
         </p>
       </div>
       <div className='flex flex-col py-4 gap-y-4'>
-      <RecoilRoot>
-        <Firstnamecounter/>
-        <Secondnamecounter/>
-        <Usernamecounter/>
-        <Passwordcounter/>
-      </RecoilRoot>
+      <div>
+          <p className='font-bold flex px-3 pb-3'>First Name</p>
+          <input type='text' value={firstName} onChange={(e)=>{
+            setFirstName(e.target.value);
+          }} className='rounded-md py-1 flex ml-4 pl-6'/>
+          </div>
+        
+          <div>
+          <p className='font-bold flex px-3 pb-3'>Last Name</p>
+        <input type='text' value={lastName} onChange={(e)=>{
+            setLastName(e.target.value);
+          }} className='rounded-md py-1 flex ml-4 pl-6'/>
+          </div>
+          <div>
+          <p className='font-bold flex px-3 pb-3'>Email</p>
+          <input type='text'value={password} onChange={(e)=>{
+            setPassword(e.target.value)
+          }} className='rounded-md py-1 flex ml-4 pl-6'/>
+          </div>
+          <div className='pb-4 '>
+          <p className='font-bold flex px-3 pb-3'>Password</p>
+          <input type='text' value={username} onChange={(e)=>{
+            setUsername(e.target.value)
+          }} className='rounded-md py-1 flex ml-4 pl-6'/>
+        </div>
+      
 
-        <div>
-        <button className='bg-black rounded-md w-60 justify-center h-8 place-self-center text-white' >sign up</button>
+        <div className='flex flex-col items-center'>
+        <button className='bg-black rounded-md w-60 justify-center h-8 place-self-center text-white' onClick={()=>{
+          async () => {
+            const response = await Axios.post("http://localhost:3000/api/v1/user/signup", {
+              username,
+              firstName,
+              lastName,
+              password
+            });
+            localStorage.setItem("token", response.data.token)
+            Navigate("/dashboard")
+          }
+        }} >sign up</button>
       
       <p> Already have an account? <Link  className='font-bold underline' to="/signin">Log in</Link></p>
         </div>
@@ -38,55 +71,7 @@ export default function Signup() {
   )
 
 }
-function Firstnamecounter(){
-  const[text,setText]=useRecoilState(FirstName);
-  const onChange= (event)=>{
-    setText(event.target.value);
-  };
-  return(
-<div>
-          <p className='font-bold flex px-3 pb-3'>First Name</p>
-          <input type='text' value={text} onChange={onChange} className='rounded-md py-1 flex ml-4 pl-6'/>
-          </div>
-  );
-}
-function Secondnamecounter(){
-  const[text,setText]=useRecoilState(SecondName);
-  const onChange= (event)=>{
-    setText(event.target.value);
-  };
-  return(
-<div>
-          <p className='font-bold flex px-3 pb-3'>Last Name</p>
-          <input type='text' value={text} onChange={onChange} className='rounded-md py-1 flex ml-4 pl-6'/>
-          </div>
-  )
-}
-function Usernamecounter(){
-  const[text,setText]=useRecoilState(Username);
-  const onChange= (event)=>{
-    setText(event.target.value);
-  };
-  return(
-    <div>
-          <p className='font-bold flex px-3 pb-3'>Email</p>
-          <input type='text'value={text} onChange={onChange} className='rounded-md py-1 flex ml-4 pl-6'/>
-          </div>
-  )
-}
-function Passwordcounter(){
-  const[text,setText]=useRecoilState(Password);
-  const onChange= (event)=>{
-    setText(event.target.value);
-  };
-  return(
-    <div className='pb-4 '>
-          <p className='font-bold flex px-3 pb-3'>Password</p>
-          <input type='text' value={text} onChange={onChange} className='rounded-md py-1 flex ml-4 pl-6'/>
-        </div>
-     
-  )
-}
+
 
 
 
